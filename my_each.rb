3f242322011d9556e5a2cfc3262bad007c90bc5a
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-08 01:17:32 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+def my_each(array)
+  i = 0
+while i < array.count do 
+  yield array[i]
+  i += 1  
+  
+  
+end 
+array
+  # put argument(s) here
   # code here
 end
